e3f08854913dceac570ba8a3254015587e257e6a
litbear <litbear@foxmail.com>
2018-08-04 17:16:34 +0800

    完成 基于数组的队列

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayDeque<T> {
 
@@ -28,42 +29,19 @@ public class ArrayDeque<T> {
         items = (T[]) new Object[DEFAULT_ARRAY_SIZE];
     }
 
-    private boolean isAlmostFull() {
-        return true;
-    }
-
-    private boolean isAlmostEmpty() {
-        return true;
-    }
-
-    private void grow() {
-
-    }
-
-    private void shrink() {
-
-    }
-
-    public void addFirst(T item) {
-
-    }
-
-    public T getLast() {
-        return null;
-    }
-
-    public void addLast(T item) {
-
-    }
-
-    public boolean isEmpty() {
-        return true;
-    }
 
     private static int inc(int index, int length) {
         index += 1;
         if (index >= length) {
             index = 0;
+        }
+        return index;
+    }
+
+    private static int inc(int index, int distance, int length) {
+        index += distance;
+        if (index >= length) {
+            index = index - length;
         }
         return index;
     }
@@ -81,34 +59,134 @@ public class ArrayDeque<T> {
         return size < 0 ? (size + length) : size;
     }
 
+    private boolean isAlmostFull() {
+        return size() > (items.length << 1);
+    }
+
+    private boolean isAlmostEmpty() {
+        return size() < (items.length >> 2);
+    }
+
+    private void grow() {
+        T[] newItems = (T[]) new Object[items.length << 1];
+        arrayCopy(items, newItems);
+        items = newItems;
+    }
+
+    private void shrink() {
+        T[] newItems = (T[]) new Object[items.length >> 1];
+        arrayCopy(items, newItems);
+        items = newItems;
+    }
+
+    private void arrayCopy(T[] src, T[] dst) {
+        int length = size();
+        if ((tail - head) < 0) {
+            System.arraycopy(src, head, dst, 0, (items.length - head));
+            System.arraycopy(src, 0, dst, (items.length - head), tail);
+        } else {
+            System.arraycopy(src, head, dst, 0, length);
+        }
+        head = 0;
+        tail = length;
+    }
+
+    public void addFirst(T item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        head = dec(head, items.length);
+        items[head] = item;
+        if (isAlmostFull()) {
+            grow();
+        }
+    }
+
+    public T getFirst() {
+        return items[head];
+    }
+
+    public T removeFirst() {
+        T item = getFirst();
+        // if 条件很重要！ 如果不判断，则会造成first一直前移
+        if(item == null) {
+            return null;
+        }
+        items[head] = null;
+        head = inc(head, items.length);
+        if(isAlmostEmpty()) {
+            shrink();
+        }
+        return item;
+    }
+
+    public void addLast(T item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        items[tail] = item;
+        tail = inc(tail, items.length);
+        if (isAlmostFull()) {
+            grow();
+        }
+    }
+
+    public T getLast() {
+        /* 使用 dec 保证在边界条件 初始数组长度设为1 的时候
+         *   ，tail 为0，而 tail 的前一个元素不至于成为 -1
+         */
+        int index = dec(tail, items.length);
+        return items[index];
+    }
+
+    public T removeLast() {
+        T item = getLast();
+        // if 条件很重要！ 如果不判断会造成 tail 一直后移
+        if(item == null) {
+            return null;
+        }
+        tail = dec(tail, items.length);
+        items[tail] = null;
+        if (isAlmostEmpty()) {
+            shrink();
+        }
+        return item;
+
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
     public int size() {
         return sub(tail, head, items.length);
     }
 
-    public T getFirst() {
-        return null;
-    }
-
-
     public void printDeque() {
+        System.out.println(this);
     }
-
-    public T removeFirst() {
-        return null;
-    }
-
-    @SuppressWarnings("Duplicates")
-    public T removeLast() {
-        return null;
-
-    }
-
+    
     public T get(int index) {
-        return null;
+        if (index < 0 || index > size() - 1) {
+            return null;
+        }
+        int i = inc(head, index, items.length);
+        return items[i];
     }
 
     @Override
     public String toString() {
-        return null;
+        Stream<T> stream;
+        if ((tail - head) < 0) {
+            stream = Arrays
+                    .stream(Arrays.copyOfRange(items, head, (items.length - 1)));
+            stream = Stream.concat(stream, Arrays
+                    .stream(Arrays.copyOfRange(items, 0, tail)));
+        } else {
+            stream = Arrays
+                    .stream(Arrays.copyOfRange(items, head, size()));
+        }
+        String string = stream.map(T::toString).collect(Collectors.joining(", "));
+        return getClass().getName() + "<" + string + ">";
     }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class ArrayDequeTest {
 
     private final static class Man{
@@ -18,13 +21,193 @@ public class ArrayDequeTest {
     }
 
     @Test
-    public void testPrint() {
-        ArrayDeque arrayDeque = new ArrayDeque(20);
-        arrayDeque.addLast(new Man("Ann", 1));
-        arrayDeque.addLast(new Man("Bob", 2));
-        arrayDeque.addLast(new Man("Candy", 3));
-        arrayDeque.addLast(new Man("Donald", 4));
-        arrayDeque.addLast(new Man("Ella", 5));
-        arrayDeque.printDeque();
+    public void testAddFirst() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(20);
+        arrayDeque.addFirst(new Man("Ann", 1));
+        arrayDeque.addFirst(new Man("Bob", 2));
+        arrayDeque.addFirst(new Man("Candy", 3));
+        arrayDeque.addFirst(new Man("Donald", 4));
+        arrayDeque.addFirst(new Man("Ella", 5));
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testRemoveFirstNull() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man m = arrayDeque.removeFirst();
+        assertNull(m);
+        m = arrayDeque.removeFirst();
+        assertNull(m);
+        m = arrayDeque.removeFirst();
+        assertNull(m);
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        arrayDeque.addFirst(ann);
+        arrayDeque.addFirst(bob);
+        arrayDeque.addFirst(candy);
+        arrayDeque.addFirst(donald);
+        arrayDeque.addFirst(ella);
+        Man m = arrayDeque.removeFirst();
+        assertEquals(ella, m);
+        m = arrayDeque.removeFirst();
+        assertEquals(donald, m);
+        m = arrayDeque.removeFirst();
+        assertEquals(candy, m);
+        arrayDeque.removeFirst();
+        arrayDeque.removeFirst();
+        arrayDeque.removeFirst();
+        arrayDeque.removeFirst();
+        arrayDeque.removeFirst();
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testGetFirst() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        arrayDeque.addFirst(ann);
+        arrayDeque.addFirst(bob);
+        arrayDeque.addFirst(candy);
+        arrayDeque.addFirst(donald);
+        arrayDeque.addFirst(ella);
+        arrayDeque.removeFirst();
+        Man m = arrayDeque.getFirst();
+        assertEquals(donald, m);
+        arrayDeque.removeFirst();
+        m = arrayDeque.getFirst();
+        assertEquals(candy, m);
+        m = arrayDeque.removeFirst();
+        assertEquals(candy, m);
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testAddLast() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        Man frank = new Man("frank", 6);
+        arrayDeque.addLast(candy);
+        arrayDeque.addLast(donald);
+        arrayDeque.addFirst(bob);
+        arrayDeque.addLast(ella);
+        arrayDeque.addFirst(ann);
+        arrayDeque.addLast(frank);
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testRemoveLastNull() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man m = arrayDeque.removeLast();
+        assertNull(m);
+        m = arrayDeque.removeLast();
+        assertNull(m);
+        m = arrayDeque.removeLast();
+        assertNull(m);
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testRemoveLast() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        arrayDeque.addLast(ann);
+        arrayDeque.addLast(bob);
+        arrayDeque.addLast(candy);
+        arrayDeque.addLast(donald);
+        arrayDeque.addLast(ella);
+        Man m = arrayDeque.removeLast();
+        assertEquals(ella, m);
+        m = arrayDeque.removeLast();
+        assertEquals(donald, m);
+        m = arrayDeque.removeLast();
+        assertEquals(candy, m);
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void testGetLast() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        arrayDeque.addLast(ann);
+        arrayDeque.addLast(bob);
+        arrayDeque.addLast(candy);
+        arrayDeque.addLast(donald);
+        arrayDeque.addLast(ella);
+        System.out.println(arrayDeque);
+        arrayDeque.removeLast();
+        System.out.println(arrayDeque);
+        Man m = arrayDeque.getLast();
+        assertEquals(donald, m);
+        arrayDeque.removeFirst();
+        System.out.println(arrayDeque);
+        m = arrayDeque.getLast();
+        assertEquals(donald, m);
+        m = arrayDeque.removeLast();
+        assertEquals(donald, m);
+        System.out.println(arrayDeque);
+    }
+
+    @Test
+    public void  testGetIndex() {
+        ArrayDeque<Man> arrayDeque = new ArrayDeque<>(10);
+        Man ann = new Man("Ann", 1);
+        Man bob = new Man("Bob", 2);
+        Man candy = new Man("Candy", 3);
+        Man donald = new Man("Donald", 4);
+        Man ella = new Man("Ella", 5);
+        arrayDeque.addLast(candy);
+        arrayDeque.addFirst(bob);
+        arrayDeque.addFirst(ann);
+        arrayDeque.addLast(donald);
+        arrayDeque.addLast(ella);
+
+        Man m = arrayDeque.get(8);
+        assertNull(m);
+
+        m = arrayDeque.get(0);
+        assertEquals(m, ann);
+        m = arrayDeque.get(1);
+        assertEquals(m, bob);
+        m = arrayDeque.get(2);
+        assertEquals(m, candy);
+        m = arrayDeque.get(3);
+        assertEquals(m, donald);
+        m = arrayDeque.get(4);
+        assertEquals(m, ella);
+
+        System.out.println(arrayDeque);
     }
 }
