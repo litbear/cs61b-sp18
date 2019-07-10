9b3856f0f23e112cbf5313b9c2d036a55c686185
litbear <litbear@foxmail.com>
2019-07-10 17:31:44 +0800

    添加 Dijkstra算法

---
+++ from-scratch/src/io/litbear/datastructure/digraph/DijkstraSP.java
@@ -0,0 +1,165 @@
+package io.litbear.datastructure.digraph;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import io.litbear.datastructure.heap.IndexMinPQ;
+
+public class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        // 负权重边检验
+        for (DirectedEdge e : G.edges()) {
+            if (e.weight() < 0)
+                throw new IllegalArgumentException("edge " + e + " has negative weight");
+        }
+
+        // 初始化属性
+        distTo = new double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+
+        // 检验起点合法性
+        validateVertex(s);
+
+        // Dijkstra初始化
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        pq = new IndexMinPQ<>(G.V());
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.minIndex();
+            pq.delMin();
+            for (DirectedEdge e : G.adj(v)) {
+                relax(G, e);
+            }
+        }
+
+        assert check(G, s);
+    }
+
+    // APIs
+    public double distTo(int v) {
+        validateVertex(v);
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return distTo[v] != Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        validateVertex(v);
+        if (distTo[v] == Double.POSITIVE_INFINITY) return null;
+        Stack<DirectedEdge> stack = new Stack<>();
+        while (distTo[v] != 0.0) {
+            stack.push(edgeTo[v]);
+            v = edgeTo[v].from();
+        }
+        return stack;
+    }
+
+    // utils
+    private void relax(EdgeWeightedDigraph G, DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w)) pq.changeKey(w, distTo[w]);
+            else pq.insert(w, distTo[w]);
+        }
+    }
+
+    private void validateVertex(int v) {
+        int V = distTo.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    // check optimality conditions:
+    // (i) for all edges e:            distTo[e.to()] <= distTo[e.from()] + e.weight()
+    // (ii) for all edge e on the SPT: distTo[e.to()] == distTo[e.from()] + e.weight()
+    private boolean check(EdgeWeightedDigraph G, int s) {
+
+        // check that edge weights are nonnegative
+        for (DirectedEdge e : G.edges()) {
+            if (e.weight() < 0) {
+                System.err.println("negative edge weight detected");
+                return false;
+            }
+        }
+
+        // check that distTo[v] and edgeTo[v] are consistent
+        if (distTo[s] != 0.0 || edgeTo[s] != null) {
+            System.err.println("distTo[s] and edgeTo[s] inconsistent");
+            return false;
+        }
+        for (int v = 0; v < G.V(); v++) {
+            if (v == s) continue;
+            if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+                System.err.println("distTo[] and edgeTo[] inconsistent");
+                return false;
+            }
+        }
+
+        // check that all edges e = v->w satisfy distTo[w] <= distTo[v] + e.weight()
+        for (int v = 0; v < G.V(); v++) {
+            for (DirectedEdge e : G.adj(v)) {
+                int w = e.to();
+                if (distTo[v] + e.weight() < distTo[w]) {
+                    System.err.println("edge " + e + " not relaxed");
+                    return false;
+                }
+            }
+        }
+
+        // check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] + e.weight()
+        for (int w = 0; w < G.V(); w++) {
+            if (edgeTo[w] == null) continue;
+            DirectedEdge e = edgeTo[w];
+            int v = e.from();
+            if (w != e.to()) return false;
+            if (distTo[v] + e.weight() != distTo[w]) {
+                System.err.println("edge " + e + " on shortest path not tight");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Unit tests the {@code DijkstraSP} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+        int s = Integer.parseInt(args[1]);
+
+        // compute shortest paths
+        DijkstraSP sp = new DijkstraSP(G, s);
+
+
+        // print shortest path
+        for (int t = 0; t < G.V(); t++) {
+            if (sp.hasPathTo(t)) {
+                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+                for (DirectedEdge e : sp.pathTo(t)) {
+                    StdOut.print(e + "   ");
+                }
+                StdOut.println();
+            }
+            else {
+                StdOut.printf("%d to %d         no path\n", s, t);
+            }
+        }
+    }
+}
