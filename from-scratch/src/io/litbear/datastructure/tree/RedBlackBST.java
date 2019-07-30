6d73a7ea429d7489976cba5f60960e2f6cd89fca
litbear <litbear@foxmail.com>
2019-07-30 18:24:19 +0800

    2019-07-30

--- from-scratch/src/io/litbear/datastructure/tree/RedBlackBST.java
+++ from-scratch/src/io/litbear/datastructure/tree/RedBlackBST.java
@@ -11,117 +11,127 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         private Value value;
         private Node left, right;
         private boolean color;
+        private int size;
 
-        public Node(Key key, Value value, boolean color) {
+        public Node(Key key, Value value, boolean color, int size) {
             this.key = key;
             this.value = value;
             this.color = color;
+            this.size = size;
         }
     }
 
     public RedBlackBST() {
     }
 
-    private boolean isRed(Node node) {
-        if (node == null) return false;
-        return node.color == RED;
+    // utils
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.size;
     }
 
     /**
-     * 左旋红黑树
-     *
-     * @param h 待操作节点
-     * @return 操作后的节点
-     */
-    private Node rotateLeft(Node h) {
-        // null 检查根本不用加，因为put方法内已经使用isRed(node.right)为true对h的右节点的非空性进行了检查
-        if (h == null) return null;
-        Node x = h.right;
-        h.right = x.left;
-        x.left = h;
-        x.color = h.color;
-        h.color = RED;
-        return x;
-    }
-
-    /**
-     * 右旋红黑树
-     *
-     * @param h 待操作节点
-     * @return 操作后的节点
+     * 右旋也是涉及两个节点的操作
+     * @param h
+     * @return
      */
     private Node rotateRight(Node h) {
-        // null 检查根本不用加，因为put方法内已经使用isRed()为true对h的非空性进行了检查
-        if (h == null) return null;
+        // 这个并不是判定条件，只是进入本函数的前置条件而已
+        assert (h != null) && isRed(h.left);
         Node x = h.left;
         h.left = x.right;
         x.right = h;
-        x.color = h.color;
-        h.color = RED;
+
+        x.color = x.right.color; // 恢复原位置颜色
+        h.color = RED; // 等待颜色翻转
+
+        x.size = h.size;
+        h.size = size(h.left) + size(h.right) + 1;
+
         return x;
     }
 
     /**
-     * 反转指定节点与其左右子节点的颜色
-     *
-     * @param node 待操作节点
+     * 左旋其实只是涉及两个节点的操作，
+     * 因此颜色上不必顾及h的父节点，
+     * @param h
+     * @return
      */
-    private void flipColors(Node node) {
-        // null 检查根本不用加，因为isRed方法已经将null识别为BLACK了
-        if (node == null) return;
-        node.color = !node.color;
-        node.right.color = !node.right.color;
-        node.left.color = !node.left.color;
+    private Node rotateLeft(Node h) {
+        assert (h != null) && isRed(h.right);
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color; // 恢复原位置颜色
+        h.color = RED;
+
+        x.size = h.size;
+        h.size = size(h.left) + size(h.right) + 1;
+
+        return x;
     }
 
-    // ------------------GET----------------------
-    public boolean isContain(Key key) {
-        if (key == null) throw new IllegalArgumentException("calls contains() with a null key");
-        return get(key) != null;
+    private void flipColors(Node h) {
+        // h must have opposite color of its two children
+        assert (h != null) && (h.left != null) && (h.right != null);
+        assert (!isRed(h) && isRed(h.left) && isRed(h.right))
+                || (isRed(h) && !isRed(h.left) && !isRed(h.right));
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+
+    //-------------------------------------------
+    // api
+    public int size() {
+        return size(root);
+    }
+
+    public boolean isEmpty() {
+        return root == null;
     }
 
     public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("calls get() with a null key");
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
         return get(root, key);
     }
 
-    private Value get(Node node, Key key) {
-        while (node != null) {
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0) {
-                node = node.left;
-            } else if (cmp > 0) {
-                node = node.right;
-            } else {
-                return node.value;
-            }
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int compare = key.compareTo(x.key);
+            if (compare < 0) x = x.left;
+            else if (compare > 0) x = x.right;
+            else return x.value;
         }
         return null;
     }
 
-    // ------------------PUT---------------------
     public void put(Key key, Value value) {
-        if (key == null) throw new IllegalArgumentException("calls put() with a null key");
+        if (null == value) {
+            throw new IllegalArgumentException();
+        }
         root = put(root, key, value);
         root.color = BLACK;
     }
 
     private Node put(Node node, Key key, Value value) {
-        if (node == null) return new Node(key, value, RED);
-        int cmp = key.compareTo(node.key);
-        if (cmp < 0) {
-            node.left = put(node.left, key, value);
-        } else if(cmp > 0) {
-            node.right = put(node.right, key, value);
-        } else {
-            node.value = value;
-        }
+        if (null == node) return new Node(key, value, RED, 1);
+        int cmp = node.key.compareTo(key);
+        if (cmp < 0) node.left = put(node.left, key, value);
+        else if(cmp > 0) node.right = put(node.right, key, value);
+        else node.value = value;
 
-        // 首先 if (node == null) return 这一步拦截了所有的null，执行到这一行以下的node都是Node实例
-        // 其次 既然node都是Node实例了，那么node.left.left就不会产生NullPointer
         if (!isRed(node.left) && isRed(node.right)) node = rotateLeft(node);
         if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
         if (isRed(node.left) && isRed(node.right)) flipColors(node);
+
+        node.size = size(node.left) + size(node.right) + 1;
 
         return node;
     }
